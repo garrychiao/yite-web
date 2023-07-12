f8c19935445df39fd571d08aff1ea4cddf05f73d
garry.chiao <garry.chiao@full-stack-artisan.com>
2023-07-12 17:03:14 +0700

    feat(app): template page added

---
+++ src/page/template/TemplatePage.js
@@ -0,0 +1,21 @@
+// import { Suspense } from 'react';
+import styled from 'styled-components';
+import { Row, Col, Typography, Carousel, Image } from 'antd';
+import color from 'shared/style/color';
+import i18n from 'i18next';
+import { ArrowRightOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+import { Section } from 'shared/layout';
+
+const { Title } = Typography;
+
+export default function TemplatePage() {
+
+  return (
+    <Section.Container>
+      <Section>
+        hello world
+      </Section>
+    </Section.Container>
+  );
+}
