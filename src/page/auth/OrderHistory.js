ef63a95ecf0a13edcf2007bbb3dc6df85655b64a
garry.chiao <garry.chiao@full-stack-artisan.com>
2023-07-14 00:19:23 +0700

    feat(userOrder): history list, buy again setup

--- src/page/auth/OrderHistory.js
+++ src/page/auth/OrderHistory.js
@@ -1,61 +1,178 @@
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 // import styled from 'styled-components';
-import { Table, Row, Col, Divider, Typography } from 'antd';
+import { Button, Space, Table, List, Image, Row, Col, Divider, Typography, notification } from 'antd';
 import dayjs from 'dayjs';
 import { useAuthUser } from 'react-auth-kit';
 import { Section } from 'shared/layout';
+import { orderApi, cartApi } from 'page/api';
+import { useRequest } from 'ahooks';
+import getSysFileUrl from 'utils/apiSysFiles';
+import CurrencyFormat from 'react-currency-format';
+import { useNavigate } from 'react-router-dom';
+import { useCart } from 'shared/cart';
 
-const { Title } = Typography;
+const { Title, Text } = Typography;
 
 export default function OrderHistory() {
 
   const auth = useAuthUser();
   const user = useMemo(() => auth() || {}, [auth]);
+  const navigate = useNavigate();
+  const { fetchCart } = useCart();
+  const [loadingBuyAgain, setLoadingBuyAgain] = useState(false);
+
+  const { data, loading:loadingList } = useRequest(() => orderApi.list());
+  const tableData = useMemo(() => data?.rows || [], [data])
+  // console.log(tableData);
+
+  // const { run: addToCart, loading: loadingAddToCart } = useRequest(({ payload }) => cartApi.add({ payload }), {
+  //   manual: true,
+  //   onSuccess: (data) => {
+  //     // console.log(data)
+  //     fetchCart();
+  //     notification.success({
+  //       message: '成功加入購物車'
+  //     })
+  //     navigate('/cart')
+  //   }, onError: (err) => {
+  //     console.error(err);
+  //     notification.error({
+  //       message: '發生錯誤'
+  //     })
+  //   }
+  // });
 
   const columns = [
-  {
-    title: '訂單編號',
-    dataIndex: 'name',
-    key: 'name',
-  },
-  {
-    title: '日期',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: '訂單狀態',
-    dataIndex: 'address',
-    key: 'address',
-  },
-  {
-    title: '訂單狀態',
-    dataIndex: 'status',
-    key: 'status',
-  },
-  {
-    title: '品項',
-    dataIndex: 'items',
-    key: 'items',
-  },
-  {
-    title: '金額',
-    dataIndex: 'amount',
-    key: 'amount',
-  },
-  {
-    title: '操作',
-    dataIndex: 'actions',
-    key: 'actions',
-  },
-];
+    {
+      title: '訂單編號',
+      dataIndex: 'id',
+      key: 'id',
+      width: 150,
+      // render: (value) => <Paragraph ellipsis>{}</Paragraph>
+    },
+    {
+      title: '日期',
+      dataIndex: 'createdAt',
+      key: 'createdAt',
+      width: 150,
+      render: (value) => dayjs(value).format('YYYY-MM-DD HH:mm:ss')
+    },
+    {
+      title: '訂單狀態',
+      dataIndex: 'status',
+      key: 'status',
+    },
+    {
+      title: '品項',
+      dataIndex: 'orderItems',
+      key: 'orderItems',
+      width: 400,
+      render: (targetItems) => (<List
+        dataSource={targetItems}
+        renderItem={(item, index) => (
+          <List.Item>
+            <List.Item.Meta
+              avatar={
+                <Image width={100} preview={false} src={getSysFileUrl(item.images[0].imageSysFileId)} />
+              }
+              title={item.product.productName}
+              description={<Space direction='vertical'>
+                <Text>規格：{item.selectedProductSpecs.length ? item.selectedProductSpecs.map(item => item.itemName).join('、') : <>無</>}</Text>
+                <Text>單價：
+                  <CurrencyFormat
+                    value={item.price}
+                    thousandSeparator={true}
+                    prefix={'$'}
+                    displayType='text'
+                  />
+                </Text>
+                <Text>數量：
+                  <CurrencyFormat
+                    value={item.qty}
+                    thousandSeparator={true}
+                    displayType='text'
+                  />
+                </Text>
+                <Text>合計：
+                  <CurrencyFormat
+                    value={item.price * item.qty}
+                    thousandSeparator={true}
+                    prefix={'$'}
+                    displayType='text'
+                  />
+                </Text>
+              </Space>}
+            />
+          </List.Item>
+        )}
+      />)
+    },
+    {
+      title: '金額',
+      dataIndex: 'totalPrice',
+      key: 'totalPrice',
+      render: (value) => (<CurrencyFormat
+        value={value}
+        thousandSeparator={true}
+        prefix={'$'}
+        displayType='text'
+      />)
+    },
+    {
+      title: '操作',
+      dataIndex: 'actions',
+      key: 'actions',
+      render: (_, order) => (<Button onClick={() => { onBuyAgain(order) }}>
+        再買一次
+      </Button>)
+    },
+  ];
+
+  const onBuyAgain = async (order) => {
+    const { orderItems } = order;
+    console.log(orderItems);
+    setLoadingBuyAgain(true);
+    try {
+      await Promise.all(orderItems.map(item => {
+        const payload = {
+          userId: auth().id,
+          productId: item.productId,
+          qty: item.qty,
+          selectedProductSpecs: item.selectedProductSpecs.map(item => ({
+            itemName: item.itemName,
+            productSpecId: item.productSpecId,
+          }))
+        }
+        return cartApi.add({payload})
+      }))
+      fetchCart();
+      notification.success({
+        message: '成功加入購物車'
+      })
+      navigate('/cart')
+
+    } catch (err) {
+      notification.error({
+        message: '發生錯誤'
+      })
+      console.error(err)
+    } finally {
+      setLoadingBuyAgain(false);
+    }
+  }
+
+  const loading = loadingBuyAgain || loadingList;
 
   return (
     <Section.Container>
       <Section>
         <Title level={4}>訂單記錄</Title>
         <Divider />
-        <Table data={[]} columns={columns} />
+        <Table
+          loading={loading}
+          rowKey='id'
+          dataSource={tableData}
+          columns={columns} />
       </Section>
     </Section.Container>
   );
