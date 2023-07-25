a8c5eb459bb91b4860adc441238929dba015116e
garry.chiao <garry.chiao@full-stack-artisan.com>
2023-07-26 02:21:36 +0800

    feat(ComfirmOrder): Receiver info added

--- src/page/cart/ConfirmOrderPage.js
+++ src/page/cart/ConfirmOrderPage.js
@@ -1,7 +1,7 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState, useRef } from 'react';
 import styled from 'styled-components';
 import _ from 'lodash';
-import { Divider, Row, Col, Typography, Button, Image, Table, notification } from 'antd';
+import { Input, Checkbox, Divider, Row, Col, Typography, Button, Image, Table, notification, Form, Radio, Space } from 'antd';
 import color from 'shared/style/color';
 import i18n from 'i18next';
 import { ArrowRightOutlined } from '@ant-design/icons';
@@ -14,6 +14,7 @@ import CurrencyFormat from 'react-currency-format';
 import { useAuthUser } from 'react-auth-kit';
 import { orderApi } from 'page/api';
 import { useRequest } from 'ahooks';
+import { basicFormProps, FormRow, FormCol } from 'shared/form';
 
 const { Title } = Typography;
 
@@ -21,8 +22,16 @@ export default function ConfirmOrderPage() {
   const [paymentData, setPaymentData] = useState({})
   const [paymentUrl, setPaymentUrl] = useState('')
 
+  const ORDERER = 'orderer';
+  const RECEIVER = 'receiver'
+  const [formOrderer] = Form.useForm();
+  const [form] = Form.useForm();
+  const formRef = useRef();
+  const [sameAsOrderer, setSameAsOrderer] = useState(false);
+
   const { cart, fetchCart, selectedItems, setSelectedItems, getSelectedFromLocal } = useCart();
   const auth = useAuthUser()
+  console.log(auth())
 
   const { id } = useParams();
   const navigate = useNavigate();
@@ -155,6 +164,42 @@ export default function ConfirmOrderPage() {
 
   const loading = loadingCreateOrder;
 
+  const onCheckSameAsOrderer = async (value) => {
+    console.log(value)
+    const checked = value?.target?.checked;
+
+    if (checked) {
+      const formValues = form.getFieldsValue();
+      const { orderer } = formValues;
+      console.log(Object.keys(orderer).map(key => [ORDERER, key]))
+      try {
+        await form.validateFields(Object.keys(orderer).map(key => [ORDERER, key]));
+        setSameAsOrderer(checked);
+        form.setFieldsValue({
+          [RECEIVER]: orderer
+        })
+      } catch (err) {
+        console.error(err);
+        notification.warning({
+          message: '請確實填寫訂購人資料'
+        })
+      }
+    } else {
+      setSameAsOrderer(checked);
+    }
+  }
+
+  useEffect(() => {
+    if (formRef?.current) {
+      form.setFieldsValue({
+        [ORDERER]: {
+          name: auth().displayName,
+          ...auth()
+        }
+      })
+    }
+  }, [])
+
   return (
     <>
       <Section.Container>
@@ -190,6 +235,172 @@ export default function ConfirmOrderPage() {
               </Table.Summary>
             )}
           />
+          <Divider />
+          {/* order creator data */}
+          <Form
+            ref={formRef}
+            form={form}
+            initialValues={{ ...auth() }}
+            {...basicFormProps}>
+            <Row gutter={40}>
+              <StyledFormContainer lg={12}>
+                <Title level={4} style={{ marginTop: 0, marginBottom: 20 }}>訂購人資料</Title>
+                <FormRow>
+                  <FormCol>
+                    <Form.Item
+                      name={[ORDERER, 'name']}
+                      label='姓名'
+                      rules={[
+                        { required: true, message: '姓名為必填' },
+                      ]}
+                    >
+                      <Input placeholder='姓名為必填' autoComplete='name' />
+                    </Form.Item>
+                  </FormCol>
+                  <FormCol>
+                    <Form.Item
+                      name={[ORDERER, 'phone']}
+                      label='聯絡電話'
+                      rules={[
+                        { required: true, message: '聯絡電話為必填' },
+                      ]}
+                    >
+                      <Input placeholder='聯絡電話為必填' autoComplete='phone' />
+                    </Form.Item>
+                  </FormCol>
+                </FormRow>
+                <FormRow>
+                  <FormCol>
+                    <Form.Item
+                      name={[ORDERER, 'email']}
+                      label='Email'
+                      rules={[
+                        { required: true, message: 'Email為必填' },
+                      ]}
+                    >
+                      <Input placeholder='Email為必填' autoComplete='email' />
+                    </Form.Item>
+                  </FormCol>
+                  <FormCol>
+                    <Form.Item
+                      name={[ORDERER, 'zipcode']}
+                      label='郵遞區號'
+                    >
+                      <Input autoComplete='postal-code' />
+                    </Form.Item>
+                  </FormCol>
+                </FormRow>
+                <FormRow>
+                  <FormCol full>
+                    <Form.Item
+                      name={[ORDERER, 'address']}
+                      label='地址'
+                      rules={[
+                        { required: true, message: '地址為必填' },
+                      ]}
+                    >
+                      <Input placeholder='地址為必填' autoComplete='address' />
+                    </Form.Item>
+                  </FormCol>
+                </FormRow>
+              </StyledFormContainer>
+              <Col lg={12}>
+                <Row gutter={50} align='middle' style={{ marginBottom: 20 }}>
+                  <Col>
+                    <Title level={4} style={{ margin: 0 }}>收件人資料</Title>
+                  </Col>
+                  <Col>
+                    <Checkbox checked={sameAsOrderer} onChange={onCheckSameAsOrderer}>同訂購人</Checkbox>
+                  </Col>
+                </Row>
+                <FormRow>
+                  <FormCol>
+                    <Form.Item
+                      name={[RECEIVER, 'name']}
+                      label='姓名'
+                      rules={[
+                        { required: true, message: '姓名為必填' },
+                      ]}
+                    >
+                      <Input disabled={sameAsOrderer} placeholder='姓名為必填' autoComplete='name' />
+                    </Form.Item>
+                  </FormCol>
+                  <FormCol>
+                    <Form.Item
+                      name={[RECEIVER, 'phone']}
+                      label='聯絡電話'
+                      rules={[
+                        { required: true, message: '聯絡電話為必填' },
+                      ]}
+                    >
+                      <Input disabled={sameAsOrderer} placeholder='聯絡電話為必填' autoComplete='phone' />
+                    </Form.Item>
+                  </FormCol>
+                </FormRow>
+                <FormRow>
+                  <FormCol>
+                    <Form.Item
+                      name={[RECEIVER, 'email']}
+                      label='Email'
+                      rules={[
+                        { required: true, message: 'Email為必填' },
+                      ]}
+                    >
+                      <Input disabled={sameAsOrderer} placeholder='Email為必填' autoComplete='email' />
+                    </Form.Item>
+                  </FormCol>
+                  <FormCol>
+                    <Form.Item
+                      name={[RECEIVER, 'zipcode']}
+                      label='郵遞區號'
+                    >
+                      <Input disabled={sameAsOrderer} autoComplete='postal-code' />
+                    </Form.Item>
+                  </FormCol>
+                </FormRow>
+                <FormRow>
+                  <FormCol full>
+                    <Form.Item
+                      name={[RECEIVER, 'address']}
+                      label='地址'
+                      rules={[
+                        { required: true, message: '地址為必填' },
+                      ]}
+                    >
+                      <Input disabled={sameAsOrderer} placeholder='地址為必填' autoComplete='address' />
+                    </Form.Item>
+                  </FormCol>
+                </FormRow>
+              </Col>
+            </Row>
+          </Form>
+          <Divider />
+
+          <Row>
+            <Col lg={12}>
+              <Row>
+                <Col span={24}>
+                  <Title level={4} style={{ marginBottom: 20 }}>選擇配送方式</Title>
+                  <Radio.Group onChange={() => { }}>
+                    <Space direction="vertical" size={'large'}>
+                      <Radio value={1}>宅配</Radio>
+                      <Radio value={2}>超商門市自取</Radio>
+                    </Space>
+                  </Radio.Group>
+                  <Divider />
+                </Col>
+                <Col span={24}>
+                  <Title level={4} style={{ marginBottom: 20 }}>選擇付款方式</Title>
+                  <Radio.Group onChange={() => { }}>
+                    <Space direction="vertical" size={'large'}>
+                      <Radio value={1}>信用卡一次付清</Radio>
+                      <Radio value={2}>超商門市貨到付款</Radio>
+                    </Space>
+                  </Radio.Group>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
           <Row justify={'space-around'} style={{ paddingTop: 50 }}>
             <Col>
               <Link to='/cart'>
@@ -209,19 +420,19 @@ export default function ConfirmOrderPage() {
         </Section>
       </Section.Container>
       <form id="payForm" action={paymentUrl} method="POST" style={{ display: 'none' }}>
-        <input name="MerchantID" value={paymentData.MerchantID} autocomplete="off" />
-        <input name="MerchantTradeNo" value={paymentData.MerchantTradeNo} autocomplete="off" />
-        <input name="MerchantTradeDate" value={paymentData.MerchantTradeDate} autocomplete="off" />
-        <input name="PaymentType" value={paymentData.PaymentType} autocomplete="off" />
-        <input name="TotalAmount" value={paymentData.TotalAmount} autocomplete="off" />
-        <input name="TradeDesc" value={paymentData.TradeDesc} autocomplete="off" />
-        <input name="ItemName" value={paymentData.ItemName} autocomplete="off" />
-        <input name="ReturnURL" value={paymentData.ReturnURL} autocomplete="off" />
-        <input name="ChoosePayment" value={paymentData.ChoosePayment} autocomplete="off" />
-        <input name="CheckMacValue" value={paymentData.CheckMacValue} autocomplete="off" />
-        <input name="EncryptType" value={paymentData.EncryptType} autocomplete="off" />
-        <input name="ClientBackURL" value={paymentData.ClientBackURL} autocomplete="off" />
-        <input name="CustomField1" value={paymentData.CustomField1} autocomplete="off" />
+        <input name="MerchantID" value={paymentData.MerchantID} autoComplete="off" />
+        <input name="MerchantTradeNo" value={paymentData.MerchantTradeNo} autoComplete="off" />
+        <input name="MerchantTradeDate" value={paymentData.MerchantTradeDate} autoComplete="off" />
+        <input name="PaymentType" value={paymentData.PaymentType} autoComplete="off" />
+        <input name="TotalAmount" value={paymentData.TotalAmount} autoComplete="off" />
+        <input name="TradeDesc" value={paymentData.TradeDesc} autoComplete="off" />
+        <input name="ItemName" value={paymentData.ItemName} autoComplete="off" />
+        <input name="ReturnURL" value={paymentData.ReturnURL} autoComplete="off" />
+        <input name="ChoosePayment" value={paymentData.ChoosePayment} autoComplete="off" />
+        <input name="CheckMacValue" value={paymentData.CheckMacValue} autoComplete="off" />
+        <input name="EncryptType" value={paymentData.EncryptType} autoComplete="off" />
+        <input name="ClientBackURL" value={paymentData.ClientBackURL} autoComplete="off" />
+        <input name="CustomField1" value={paymentData.CustomField1} autoComplete="off" />
 
         <button type="submit">
           確認訂單
@@ -231,8 +442,7 @@ export default function ConfirmOrderPage() {
   );
 }
 
-const ProductListContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
+const StyledFormContainer = styled(Col)`
+  border-right: 1px solid #d0d0d0;
 `
+
